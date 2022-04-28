42f97f591d163b7fdd210e68989de10bfb96e2df
CleitonCandiotto <cleitoncandiotto@gmail.com>
2022-04-28 13:46:54 -0300

    Configurando Template Base do Dashboard

--- apps/barbearias/apps.py
+++ apps/barbearias/apps.py
@@ -3,4 +3,4 @@ from django.apps import AppConfig
 
 class BarbeariasConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'barbearias'
+    name = 'apps.barbearias'
