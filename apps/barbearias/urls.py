0d877b44c76b10362f464613c84e9a9e5495ab16
CleitonCandiotto <cleitoncandiotto@gmail.com>
2022-05-04 17:29:23 -0300

    app profissionais e melhoria template

--- apps/barbearias/urls.py
+++ apps/barbearias/urls.py
@@ -1,26 +1,30 @@
 from django.urls import path
 
 from .views import DashboardView
-from .views import ServicosList, ClientesList
-from .views import ServicosCreate, ClientesCreate, HorarioFuncionamentoCreate
-from .views import ServicosUpdate, ClientesUpdate, HorarioFuncionamentoUpdate
-from .views import ServicosDelete, ClientesDelete
+from .views import ServicosList, ClientesList, ProfissionaisList
+from .views import ServicosCreate, ClientesCreate, HorarioFuncionamentoCreate, ProfissionaisCreate
+from .views import ServicosUpdate, ClientesUpdate, HorarioFuncionamentoUpdate, ProfissionaisUpdate
+from .views import ServicosDelete, ClientesDelete, ProfissionalDelete
 
 urlpatterns = [
     path('dashboard/', DashboardView.as_view(), name='dashboard'),
 
     path('servicos/', ServicosList.as_view(), name='servicos'),
     path('clientes/', ClientesList.as_view(), name='clientes'),
+    path('profissionais/', ProfissionaisList.as_view(), name='profissionais'),
 
     path('criar-servico/', ServicosCreate.as_view(), name='criar_servico'),
     path('criar-cliente/', ClientesCreate.as_view(), name='criar_cliente'),
+    path('criar-profissional/', ProfissionaisCreate.as_view(), name='criar_profissional'),
     path('criar-horario-atendimento/', HorarioFuncionamentoCreate.as_view(), name='criar_horario'),    
 
     path('editar-servico/<int:pk>/', ServicosUpdate.as_view(), name='editar_servico'),
     path('editar-cliente/<int:pk>/', ClientesUpdate.as_view(), name='editar_cliente'),
+    path('editar-profissional/<int:pk>/', ProfissionaisUpdate.as_view(), name='editar_profissional'),
     path('editar-horario-atendimento/<int:pk>/', HorarioFuncionamentoUpdate.as_view(), name='editar_horario'),
 
     path('excluir-servico/<int:pk>/', ServicosDelete.as_view(), name='excluir_servico'),
     path('excluir-cliente/<int:pk>/', ClientesDelete.as_view(), name='excluir_cliente'),
+    path('excluir-profissional/<int:pk>/', ProfissionalDelete.as_view(), name='excluir_profissional'),
 
 ]
